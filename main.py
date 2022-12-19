5ed19d23a61a6d1b7a8779ba70363a0c5816fff7
Marcin Kadziolka <kadziolka.marcin@outlook.com>
2022-12-19 01:50:25 +0100

    Show usage of new funcs

--- main.py
+++ main.py
@@ -1,27 +1,16 @@
-# import sys
-#
-# from PyQt6.QtCore import QSize, Qt
-# from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
-#
-#
-# # Subclass QMainWindow to customize your application's main window
-# class MainWindow(QMainWindow):
-#     def __init__(self):
-#         super().__init__()
-#
-#         self.setWindowTitle("Done")
-#         button = QPushButton("Press Me!")
-#         self.setMinimumSize(QSize(1000, 600))
-#         # Set the central widget of the Window.
-#         self.setCentralWidget(button)
-#
-#
-# app = QApplication(sys.argv)
-#
-# window = MainWindow()
-# window.show()
-#
-# app.exec()
 import func
-# tm = func.TaskManager("./file.txt")
-task = func.Task(tags=["@tag"], projects=[], priority="(A)")
+tm = func.TaskManager("./file.txt")
+projects = tm.get_tasks_by_projects()
+tags = tm.get_tasks_by_tags()
+print("Tasks by projects:")
+for k, v in projects.items():
+    print(*k)
+    print(*v, sep='\n')
+    print()
+
+print("Tasks by tags:")
+for k, v in tags.items():
+    print(*k)
+    print(*v, sep='\n')
+    print()
+
