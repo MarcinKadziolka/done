1820ef8424d3b9c2a14a8735adb446b161c689c2
Marcin Kadziolka <kadziolka.marcin@outlook.com>
2022-12-23 19:34:06 +0100

    Add simple terminal menu

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 from os import wait
 import func
 tm = func.TaskManager("./file.txt")
-projects = tm.get_tasks_by_projects()
-tags, done = tm.get_tasks_by_tags()
-tm.print_tasks_by_tags(tags)
-tm.print_tasks(done)
-# tm.print_tasks_by_projects(projects)
-print()
-result = tm.search("@home")
-print("Search fraze: @home")
-tm.print_tasks(result)
 
+
+def user_choice():
+    print("1. Show all tasks")
+    print("2. Show pending tasks")
+    print("3. Show done tasks")
+    print("4. Show tasks by project")
+    print("5. Show tasks by tags")
+    print("6. Search tasks")
+    user_input = input("Choose an option: ")
+    return user_input
+
+
+while(1):
+    user_input = user_choice()
+    print()
+    print()
+    if user_input == "1":
+        print(tm)
+    elif user_input == "2":
+        tm.print_tasks(tm.get_pending_tasks())
+    elif user_input == "3":
+        tm.print_tasks(tm.get_done_tasks())
+    elif user_input == "4":
+        pending = tm.get_pending_tasks()
+        pending_by_project = tm.get_tasks_by_projects(pending)
+        tm.print_tasks_in_dict(pending_by_project)
+    elif user_input == "5":
+        pending = tm.get_pending_tasks()
+        pending_by_tags = tm.get_tasks_by_tags(pending)
+        tm.print_tasks_in_dict(pending_by_tags)
+    elif user_input == "6":
+        search_phrase = input("Enter search phrase: ")
+        tm.print_tasks(tm.search(search_phrase))
+    print()
+    print()
