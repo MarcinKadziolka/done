10c7d34e34a0841930789f9364935f2a58baf9a9
Marcin Kadziolka <kadziolka.marcin@outlook.com>
2022-12-26 19:47:47 +0100

    Remove tests for deleted function
    check_exact_match

--- test.py
+++ test.py
@@ -53,20 +53,3 @@ class TestTask:
     def test_task_description(self):
         string = "(A) Do homework (unit 6) @school @library +homework"
         assert func.Task(string).description == "Do homework (unit 6)"
-
-
-class TestCheckExactMatch:
-    def test_check_exact_match_exact_match(self):
-        match = "keyword"
-        string = "contains keyword and should match"
-        assert func.TaskParser.check_exact_match(match, string) is not None
-
-    def test_check_exact_match_substring_match(self):
-        match = "keyword"
-        string = "contains keywordas a substring and should not match"
-        assert func.TaskParser.check_exact_match(match, string) is None
-
-    def test_check_exact_match_no_match(self):
-        match = "keyword"
-        string = "does not contain key word"
-        assert func.TaskParser.check_exact_match(match, string) is None
