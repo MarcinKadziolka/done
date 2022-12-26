e656468c3191fd16a37e4977fae6d8c059842aa6
Marcin Kadziolka <kadziolka.marcin@outlook.com>
2022-12-26 19:12:11 +0100

    Add comments Remove redundant function Move defining behaviour from kv to this file

--- gui.py
+++ gui.py
@@ -8,8 +8,6 @@ from kivy.uix.button import Button
 from kivy.uix.popup import Popup
 from kivymd.uix.boxlayout import MDBoxLayout
 from kivy.core.window import Window
-
-# import IconRightWidget
 from kivymd.uix.list import IconRightWidget
 
 import func
@@ -18,112 +16,121 @@ import func
 tm = func.TaskManager("./file.txt")
 
 
+class TaskItem(OneLineAvatarIconListItem):
+    def __init__(self, **kwargs):
+        # Upon creation adds DeleteIcon by defalt
+        # Passes self to icon so it can access task text
+        super(TaskItem, self).__init__(
+            DeleteIcon(self, icon="trash-can-outline"), **kwargs
+        )
+
+        # Passes self to popup, so it can access task text
+        self.edit_task_popup = EditTaskField(self)
+
+    def on_press(self):
+        self.edit_task_popup.open()
+
+
+class EditTaskField(Popup):
+    def __init__(self, parent_widget, **kwargs):
+        # parent_widget should be the object where popup was called from
+        # because self.parent is None
+        super(EditTaskField, self).__init__(**kwargs)
+        Window.borderless = True
+        # Hide the title
+        self.title = ""
+        self.separator_height = 0
+        self.parent_widget = parent_widget
+        self.size_hint = (0.8, 0.1)
+        self.content = MDBoxLayout(orientation="horizontal")
+
+        # on_text_validate is what happens when enter is pressed
+        self.input_field = MDTextField(
+            on_text_validate=self.accept_task_edit, text=self.parent_widget.text
+        )
+        self.content.add_widget(self.input_field)
+
+    # Changes the original text with text provided in the text field
+    # Edits task in TaskManager
+    # Updates the original text in gui with text provided
+    # Closes itsel
+    def accept_task_edit(self, *args):
+        print("Enter pressed")
+        print(f"Editing task {self.parent_widget.text} to {self.input_field.text}")
+        print(tm)
+        tm.edit_task(
+            old_task=func.Task(self.parent_widget.text),
+            new_task=func.Task(self.input_field.text),
+        )
+        self.parent_widget.text = self.input_field.text
+        self.dismiss()
+
+
 class AddTaskButton(Button):
+    """
+    Button class that has hidden popup with text field which is shown when button is released
+    """
+
     def __init__(self, **kwargs):
         super(AddTaskButton, self).__init__(**kwargs)
 
-        self.task_input_popup = AddTaskTextField(self)
+        self.task_input_popup = AddTaskTextField()
 
-    def add_task(self, *args):
+    def on_release(self):
         self.task_input_popup.open()
 
 
 class AddTaskTextField(Popup):
-    def __init__(self, my_widget, **kwargs):
-        # my_widget is now the object where popup was called from.
+    def __init__(self, **kwargs):
         super(AddTaskTextField, self).__init__(**kwargs)
         Window.borderless = True
         # Hide the title
         self.title = ""
         self.separator_height = 0
 
-        self.my_widget = my_widget
         self.size_hint = (0.8, 0.1)
-        self.content = MDBoxLayout(orientation="horizontal")
 
         self.input_field = MDTextField(on_text_validate=self.on_enter)
-        # self.save_button.bind(on_press=self.save)
+
+        self.content = MDBoxLayout(orientation="horizontal")
         self.content.add_widget(self.input_field)
 
+    # Adds the task to task manager list, updates display and closes the popup
     def on_enter(self, *args):
         print("Enter pressed")
         tm.add_task(func.Task(self.input_field.text))
         app = MDApp.get_running_app()
-        # Clear widget
         app.root.ids.mdlist.clear_widgets()
-        # Clear input field
         self.input_field.text = ""
+        # Update the task list
         for task in tm.tasks:
             app.root.ids.mdlist.add_widget(TaskItem(text=task.raw_text))
         self.dismiss()
 
-    def cancel(self, *args):
-        print("cancel")
-        self.dismiss()
-
-    pass
-
-
-class EditTaskField(Popup):
-    def __init__(self, my_widget, **kwargs):
-        # my_widget is now the object where popup was called from.
-        super(EditTaskField, self).__init__(**kwargs)
-        Window.borderless = True
-        # Hide the title
-        self.title = ""
-        self.separator_height = 0
-
-        self.my_widget = my_widget
-        self.size_hint = (0.8, 0.1)
-        self.content = MDBoxLayout(orientation="horizontal")
-
-        self.input_field = MDTextField(
-            on_text_validate=self.on_enter, text=self.my_widget.text
-        )
-        # self.save_button.bind(on_press=self.save)
-        self.content.add_widget(self.input_field)
-
-    def on_enter(self, *args):
-        print("Enter pressed")
-        print(f"Editing task {self.my_widget.text} to {self.input_field.text}")
-        print(tm)
-        tm.edit_task(
-            old_task=func.Task(self.my_widget.text),
-            new_task=func.Task(self.input_field.text),
-        )
-        self.my_widget.text = self.input_field.text
-        self.dismiss()
-
 
 class DeleteIcon(IconRightWidget):
-    def __init__(self, my_widget, **kwargs):
+    def __init__(self, parent_widget, **kwargs):
+        # parent_widget should be the object where popup was called from
+        # because self.parent is None
+        # it's passed so this class can access text from parent
         super(DeleteIcon, self).__init__(**kwargs)
-        self.my_widget = my_widget
+        self.parent_widget = parent_widget
 
     def on_press(self):
         print("Delete icon pressed")
         self.delete_task()
 
+    # Delets task from TaskManager
+    # Updates the task list in gui by displaying all tasks again without the deleted one
+    # TODO: Make it so that it doesn't display all tasks again, but just removes the deleted one
     def delete_task(self, *args):
-        print(f"Deleting task {self.my_widget.text}")
-        tm.delete_task(func.Task(self.my_widget.text))
+        print(f"Deleting task {self.parent_widget.text}")
+        tm.delete_task(func.Task(self.parent_widget.text))
         app = MDApp.get_running_app()
         # Clear widget
         app.root.ids.mdlist.clear_widgets()
         for task in tm.tasks:
             app.root.ids.mdlist.add_widget(TaskItem(text=task.raw_text))
-
-
-class TaskItem(OneLineAvatarIconListItem):
-    def __init__(self, **kwargs):
-        super(TaskItem, self).__init__(DeleteIcon(self, icon="trash-can-outline"), **kwargs)
-        self.edit_task_popup = EditTaskField(self)
-
-    def on_press(self):
-        self.edit_task_popup.open()
-
-    def open_edit_task_popup(self, *args):
-        self.task_input_popup.open()
 
 
 class TasksScrollView(ScrollView):
@@ -161,6 +168,7 @@ class SearchTextInput(MDTextField):
         for task in search_results:
             app.root.ids.mdlist.add_widget(TaskItem(text=task.raw_text))
 
+    # on_text is called everytime text in the input field is changed
     def on_text(self, instance, value):
         self.display_search_results()
 
@@ -182,4 +190,5 @@ class MainApp(MDApp):
 
 if __name__ == "__main__":
     MainApp().run()
+    # Writing everything to file after app is closed
     tm.write_file()
